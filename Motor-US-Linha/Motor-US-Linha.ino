8c666b8621e816b1729a24ebae8edda2920a574b
Jorge Liberato <liberatojorge@gmail.com>
2017-10-28 23:41:08 -0200

    Bibliotecas e melhoria em código(RFID)

--- Motor-US-Linha/Motor-US-Linha.ino
+++ Motor-US-Linha/Motor-US-Linha.ino
@@ -14,17 +14,17 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 #define sensorPD A11
 
 int referencia = 900;
-int sinal_direita = 0;
-int sinal_centro = 0;
-int sinal_esquerda = 0;
+int sinalDireita = 0;
+int sinalCentro = 0;
+int sinalEsquerda = 0;
 
 //motor 1
-int velocidadeA = 34;
+int velocidadeA = 7;
 int IN1 = 35 ;
 int IN2 = 36 ;
 
 //motor 2
-int velocidadeB = 39;
+int velocidadeB = 6;
 int IN3 = 37 ;
 int IN4 = 38 ;
 
@@ -38,243 +38,203 @@ int velocidadeD = 13;
 int IN7 = 11 ;
 int IN8 = 12 ;
 
-
 //variavel auxiliar
 int velocidade = 0;
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(BUZZER, OUTPUT);
-    pinMode(TRIGGER_PIN,OUTPUT);
-    pinMode(ECHO_PIN,INPUT);
-    pinMode(IN1,OUTPUT);
-    pinMode(IN2,OUTPUT);
-    pinMode(IN3,OUTPUT);
-    pinMode(IN4,OUTPUT);
-    pinMode(IN5,OUTPUT);
-    pinMode(IN6,OUTPUT);
-    pinMode(IN7,OUTPUT);
-    pinMode(IN8,OUTPUT);
-    pinMode(velocidadeA,OUTPUT);
-    pinMode(velocidadeB,OUTPUT);
-    pinMode(velocidadeC,OUTPUT);
-    pinMode(velocidadeD,OUTPUT);
-
+  Serial.begin(9600);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(IN5, OUTPUT);
+  pinMode(IN6, OUTPUT);
+  pinMode(IN7, OUTPUT);
+  pinMode(IN8, OUTPUT);
+  pinMode(velocidadeA, OUTPUT);
+  pinMode(velocidadeB, OUTPUT);
+  pinMode(velocidadeC, OUTPUT);
+  pinMode(velocidadeD, OUTPUT);
 }
 
-void FRENTE() {
-    // Motor 1
-    digitalWrite(IN1,HIGH); 
-    digitalWrite(IN2,LOW);
-    analogWrite(velocidadeA,130);
+void frente() {
+  // Motor 1
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(velocidadeA, 100);
 
-    // Motor 2
-    digitalWrite(IN3,HIGH);
-    digitalWrite(IN4,LOW);
-    analogWrite(velocidadeB,130);
+  // Motor 2
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(velocidadeB, 100);
 
-    // Motor 3
-    digitalWrite(IN5,HIGH);
-    digitalWrite(IN6,LOW);
-    analogWrite(velocidadeC,130);
-    
-    // Motor 4
-    digitalWrite(IN7,HIGH);
-    digitalWrite(IN8,LOW);
-    analogWrite(velocidadeD,130);
- }
- 
-void DIREITAB() {
-    // Motor 1
-    digitalWrite(IN1,HIGH); 
-    digitalWrite(IN2,LOW);
-    analogWrite(velocidadeA,130);
+  // Motor 3
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, LOW);
+  analogWrite(velocidadeC, 100);
 
-    // Motor 2
-    digitalWrite(IN3,LOW);
-    digitalWrite(IN4,HIGH);
-    analogWrite(velocidadeB,130);
+  // Motor 4
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, LOW);
+  analogWrite(velocidadeD, 100);
+}
 
-    // Motor 3
-    digitalWrite(IN5,LOW);
-    digitalWrite(IN6,HIGH);
-    analogWrite(velocidadeC,130);
-    
-    // Motor 4
-    digitalWrite(IN7,HIGH);
-    digitalWrite(IN8,LOW);
-    analogWrite(velocidadeD,130);
- }
+void direitaB() {
+  // Motor 1
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(velocidadeA, 100);
 
- void DIREITAF() {
-    // Motor 1
-    digitalWrite(IN1,HIGH); 
-    digitalWrite(IN2,LOW);
-    analogWrite(velocidadeA,130);
+  // Motor 2
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(velocidadeB, 100);
 
-    // Motor 2
-    digitalWrite(IN3,LOW);
-    digitalWrite(IN4,HIGH);
-    analogWrite(velocidadeB,100);
+  // Motor 3
+  digitalWrite(IN5, LOW);
+  digitalWrite(IN6, HIGH);
+  analogWrite(velocidadeC, 100);
 
-    // Motor 3
-    digitalWrite(IN5,LOW);
-    digitalWrite(IN6,HIGH);
-    analogWrite(velocidadeC,100);
-    
-    // Motor 4
-    digitalWrite(IN7,HIGH);
-    digitalWrite(IN8,LOW);
-    analogWrite(velocidadeD,130);
- }
+  // Motor 4
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, LOW);
+  analogWrite(velocidadeD, 100);
+}
 
-void ESQUERDAB() {
-    // Motor 1
-    digitalWrite(IN1,LOW); 
-    digitalWrite(IN2,HIGH);
-    analogWrite(velocidadeA,130);
+ void direitaF() {
+  // Motor 1
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(velocidadeA, 100);
 
-    // Motor 2
-    digitalWrite(IN3,HIGH);
-    digitalWrite(IN4,LOW);
-    analogWrite(velocidadeB,130);
+  // Motor 2
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(velocidadeB, 80);
 
-    // Motor 3
-    digitalWrite(IN5,HIGH);
-    digitalWrite(IN6,LOW);
-    analogWrite(velocidadeC,130);
-    
-    // Motor 4
-    digitalWrite(IN7,LOW);
-    digitalWrite(IN8,HIGH);
-    analogWrite(velocidadeD,130);
- }
+  // Motor 3
+  digitalWrite(IN5, LOW);
+  digitalWrite(IN6, HIGH);
+  analogWrite(velocidadeC, 80);
 
- void ESQUERDAF() {
-    // Motor 1
-    digitalWrite(IN1,LOW); 
-    digitalWrite(IN2,HIGH);
-    analogWrite(velocidadeA,100);
+  // Motor 4
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, LOW);
+  analogWrite(velocidadeD, 100);
+}
 
-    // Motor 2
-    digitalWrite(IN3,HIGH);
-    digitalWrite(IN4,LOW);
-    analogWrite(velocidadeB,130);
+void esquerdaB() {
+  // Motor 1
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(velocidadeA, 100);
 
-    // Motor 3
-    digitalWrite(IN5,HIGH);
-    digitalWrite(IN6,LOW);
-    analogWrite(velocidadeC,100);
-    
-    // Motor 4
-    digitalWrite(IN7,LOW);
-    digitalWrite(IN8,HIGH);
-    analogWrite(velocidadeD,130);
- }
+  // Motor 2
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(velocidadeB, 100);
 
-void PARAR() {
-    // Motor 1
-    digitalWrite(IN1,HIGH); 
-    digitalWrite(IN2,HIGH);
-    
-    // Motor 2
-    digitalWrite(IN3,HIGH);
-    digitalWrite(IN4,HIGH);
-    
-    // Motor 3
-    digitalWrite(IN5,HIGH);
-    digitalWrite(IN6,HIGH);
-        
-    // Motor 4
-    digitalWrite(IN7,HIGH);
-    digitalWrite(IN8,HIGH);
- }
+  // Motor 3
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, LOW);
+  analogWrite(velocidadeC, 100);
 
-void VOLTAR() {
-    // Motor 1
-    digitalWrite(IN1,LOW); 
-    digitalWrite(IN2,HIGH);
-    analogWrite(velocidadeA,130);
+  // Motor 4
+  digitalWrite(IN7, LOW);
+  digitalWrite(IN8, HIGH);
+  analogWrite(velocidadeD, 100);
+}
 
-    // Motor 2
-    digitalWrite(IN3,LOW);
-    digitalWrite(IN4,HIGH);
-    analogWrite(velocidadeB,130);
+void esquerdaF() {
+  // Motor 1
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(velocidadeA, 80);
 
-    // Motor 3
-    digitalWrite(IN5,LOW);
-    digitalWrite(IN6,HIGH);
-    analogWrite(velocidadeC,130);
-    
-    // Motor 4
-    digitalWrite(IN7,LOW);
-    digitalWrite(IN8,HIGH);
-    analogWrite(velocidadeD,130);
- }
+  // Motor 2
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(velocidadeB, 100);
+
+  // Motor 3
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, LOW);
+  analogWrite(velocidadeC, 100);
+
+  // Motor 4
+  digitalWrite(IN7, LOW);
+  digitalWrite(IN8, HIGH);
+  analogWrite(velocidadeD, 80);
+}
+
+void parar() {
+  // Motor 1
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+
+  // Motor 2
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+
+  // Motor 3
+  digitalWrite(IN5, HIGH);
+  digitalWrite(IN6, HIGH);
+
+  // Motor 4
+  digitalWrite(IN7, HIGH);
+  digitalWrite(IN8, HIGH);
+}
 
 void loop() {
-    Serial.print("Distancia: ");
-    Serial.println(sonar.ping_cm());
-    float cm = sonar.ping_cm();
-    
-    sinal_esquerda = analogRead(sensorE);
-    sinal_centro = analogRead(sensorC);
-    sinal_direita = analogRead(sensorD);
+  Serial.print("Distancia: ");
+  Serial.println(sonar.ping_cm());
+  float cm = sonar.ping_cm();
 
-    Serial.print("Nivel Esquerdo : ");
-    Serial.println(sinal_esquerda);
-    Serial.print("Nível Centro: ");
-    Serial.println(sinal_centro);
-    Serial.print("Nivel Direito : ");
-    Serial.println(sinal_direita);
-    Serial.println("");
+  sinalEsquerda = analogRead(sensorE);
+  sinalCentro = analogRead(sensorC);
+  sinalDireita = analogRead(sensorD);
 
-    if (cm < 15) {
-        Serial.println("PARAR");
-        PARAR();
-        
+  Serial.print("Nivel Esquerdo : ");
+  Serial.println(sinalEsquerda);
+  Serial.print("Nível Centro: ");
+  Serial.println(sinalCentro);
+  Serial.print("Nivel Direito : ");
+  Serial.println(sinalDireita);
+  Serial.println("");
+
+  if (cm < 15) {
+    Serial.println("PARAR");
+    parar();
+  } else {
+    Serial.print("ANDAR P/ ");
+
+    if (sinalCentro < referencia ) {
+      Serial.println("FRENTE");
+      frente();
+    } else if (sinalDireita < referencia && sinalCentro < referencia && sinalEsquerda < referencia) {
+      Serial.println("FRENTE");
+      frente();
+    } else if (sinalDireita < referencia && sinalCentro < referencia && sinalEsquerda > referencia) {
+      Serial.println("ESQUERDAF");
+      esquerdaF();
+    } else if (sinalDireita > referencia && sinalCentro < referencia && sinalEsquerda < referencia){
+      Serial.println("DIREITAF");
+      direitaF();
+    } else if (sinalDireita < referencia && sinalCentro > referencia) {
+      Serial.println("DIREITAB");
+      direitaB();
+    } else if (sinalEsquerda < referencia && sinalCentro > referencia) {
+      Serial.println("ESQUERDAB");
+      esquerdaB();
     }
-    else {
-        Serial.print("ANDAR P/ ");
-    
-        if (sinal_centro < referencia ) {
-        Serial.println("FRENTE");
-        FRENTE();
-        }
-        else if (sinal_direita < referencia && sinal_centro < referencia && sinal_esquerda < referencia) {
-        Serial.println("FRENTE");
-        FRENTE();
-        }
-        else if (sinal_direita < referencia && sinal_centro < referencia && sinal_esquerda > referencia) {
-        Serial.println("ESQUERDAF");
-        ESQUERDAF();
-        }
-        else if (sinal_direita > referencia && sinal_centro < referencia && sinal_esquerda < referencia){
-        Serial.println("DIREITAF");
-        DIREITAF();
-        }   
-        else if (sinal_direita < referencia && sinal_centro > referencia) {
-        Serial.println("DIREITAB");
-        DIREITAB();
-        }
-        else if (sinal_esquerda < referencia && sinal_centro > referencia) {
-        Serial.println("ESQUERDAB");
-        ESQUERDAB();
-        }
-        else if (sinal_direita > referencia && sinal_centro > referencia && sinal_esquerda > referencia) {
-        Serial.println("PARAR");
-        PARAR();
-        delay(1000);
-        Serial.println("VOLTAR");
-        VOLTAR();
-        delay(1500);
-        Serial.println("DIREITAB");
-        DIREITAB();
-        }
-        else {
-        Serial.println("PARAR");
-        PARAR();
-        }
-    }    
-    Serial.println("");
+      else {
+      Serial.println("PARAR");
+      parar();
+    }
+  }
+
+  Serial.println("");
 }
---
+++ rfid-mqtt/rfid-mqtt.ino
@@ -0,0 +1,184 @@
+// Carrega as bibliotecas
+#include <SPI.h>
+#include <WiFiEsp.h>
+#include <WiFiEspClient.h>
+#include <PubSubClient.h>
+#include <MFRC522.h>
+
+#define DEBUG true
+
+// WIFI
+#define SSID "Gabriel"
+#define PASSWORD "bi91654704"
+
+// MQTT
+IPAddress server(198,41,30,241); //Servidor 
+int PORT = 1883;
+
+//RFID
+
+constexpr uint8_t RST_PIN = 5;
+constexpr uint8_t SS_PIN = 53;
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+int status = WL_IDLE_STATUS;   // the Wifi radio's status
+
+// Initialize the client object and MQTT
+WiFiEspClient espClient;
+PubSubClient mqttClient(espClient);
+
+//Variáveis auxiliares MQTT
+char message_buff[100];
+char resp[20];
+
+void setup(){
+    Serial.begin(9600);
+    Serial1.begin(115200);
+    SPI.begin();
+    
+    //RFID
+    Serial.println("Configurando RFID");
+    mfrc522.PCD_Init();
+    Serial.println("Feito");
+
+    Serial.println("- Configurando modulo...");
+    sendData("AT+CWMODE=1\r\n",1000,DEBUG); // modo de operação STA
+    Serial.println("- Resetando modulo...");
+    sendData("AT+RST\r\n",1000,DEBUG); // resetar módulo
+    Serial.println("- Conectando no roteador");
+    sendData("AT+CWJAP=\"Gabriel\",\"bi91654704\"\r\n", 5000, DEBUG); // conectar na rede wifi
+
+    // initialize ESP module
+    WiFi.init(&Serial1);
+
+   // check for the presence of the shield
+    if (WiFi.status() == WL_NO_SHIELD) {
+      Serial.println("WiFi shield not present");
+      // don't continue
+      while (true);
+    }
+
+    // attempt to connect to WiFi network
+    while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to WPA SSID: ");
+      Serial.println(SSID);
+      // Connect to WPA/WPA2 network
+      status = WiFi.begin(SSID, PASSWORD);
+    }
+
+    // you're connected now, so print out the data
+    Serial.println("You're connected to the network");
+
+    //connect to MQTT server
+    mqttClient.setServer(server, PORT);
+    mqttClient.setCallback(callback); 
+}
+
+void callback(char* topic, byte* payload, unsigned int length) { 
+  int i = 0;
+
+//    Serial.print("Message arrived [");
+//    Serial.print(topic);
+//    Serial.println("] ");
+  
+  
+  if (strcmp(topic,"julinho/rota")==0){
+    for (int i=0;i<length;i++) {
+      Serial.print((char)payload[i]);
+      
+    }
+    
+    Serial.print(" - ");
+    Serial.println("ROTA = ");
+    Serial.println((char*)payload);
+  }
+//
+//  if (strcmp(topic,"julinho/#")==0){
+//    String msg = "";
+//    Serial.print("Message arrived [");
+//    Serial.print(topic);
+//    Serial.println("] ");
+//    for (int i=0;i<length;i++) {
+//      Serial.print((char)payload[i]);
+//      msg += (char)payload[i];
+//      message_buff[i] = payload[i];
+//    }
+//    message_buff[i] = '\0';
+//    String msgString = String(message_buff);
+//    Serial.println("Payload: " + msgString);
+//  }
+}
+
+String sendData(const char* command, const int timeout, boolean debug){
+  String response = "";
+  Serial1.print(command); // send the read character to the esp8266
+  long int time = millis();
+  while( (time+timeout) > millis()){
+    while(Serial1.available()){
+      // The esp has data so display its output to the serial window
+      char c = Serial1.read(); // read the next character.
+      response+=c;
+    }
+  }
+  if(debug){
+    Serial.println(response.c_str());
+  }
+  return response;
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    // Attempt to connect, just a name to identify the client
+    if (mqttClient.connect("arduinoClient")) {
+      Serial.println("connected");
+      mqttClient.publish("julinho/enable","1");
+      mqttClient.publish("julinho/sos","0");
+      mqttClient.subscribe("julinho/#");
+      //mqttClient.subscribe("julinho/dev");
+//        mqttClient.subscribe("julinho/#");
+      
+          } 
+      else {
+      Serial.print("failed, rc = ");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+
+      //  Procura tags;
+    if ( ! mfrc522.PICC_IsNewCardPresent()) { 
+    }
+
+    //  Lê a tag
+    if ( ! mfrc522.PICC_ReadCardSerial());
+    String rfidUid = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    rfidUid += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+    rfidUid += String(mfrc522.uid.uidByte[i], HEX);
+  }
+    
+  // put your main code here, to run repeatedly:
+  //int value = random(1,10);
+  if (!mqttClient.connected()) {
+    reconnect();
+  } 
+  else {
+      
+      rfidUid.toCharArray(message_buff, rfidUid.length() + 1);
+      mqttClient.publish("julinho/check", message_buff);
+      Serial.print("Message sended: ");
+      Serial.println(message_buff);
+
+    }
+    mqttClient.loop();
+    return;
+ 
+}
