57652194dad40a1edb958b8f76671bdd6bf48058
Thiago <thia.mdossantos@gmail.com>
2017-11-06 18:49:10 -0200

    :bug: fix conflicts

--- rfid-mqtt/rfid-mqtt.ino
+++ rfid-mqtt/rfid-mqtt.ino
@@ -12,11 +12,7 @@
 #define PASSWORD "bi91654704"
 
 // MQTT
-<<<<<<< HEAD:rfid-mqtt/rfid-mqtt.ino
-IPAddress server(198,41,30,241); //Servidor 
-=======
-IPAddress server(191,36,8,24); //Servidor 
->>>>>>> 80467243f5bb992124ad779c36d207021598e85a:rfid-mqtt/rfid-mqtt.ino
+IPAddress server(191,36,8,24); //Servidor
 int PORT = 1883;
 
 //RFID
@@ -34,94 +30,64 @@ PubSubClient mqttClient(espClient);
 
 //Variáveis auxiliares MQTT
 char message_buff[100];
-<<<<<<< HEAD:rfid-mqtt/rfid-mqtt.ino
-char resp[20];
-=======
->>>>>>> 80467243f5bb992124ad779c36d207021598e85a:rfid-mqtt/rfid-mqtt.ino
 
 void setup(){
-    Serial.begin(9600);
-    Serial1.begin(115200);
-    SPI.begin();
-    
-    //RFID
-    Serial.println("Configurando RFID");
-    mfrc522.PCD_Init();
-    Serial.println("Feito");
+  Serial.begin(9600);
+  Serial1.begin(115200);
+  SPI.begin();
 
-    Serial.println("- Configurando modulo...");
-    sendData("AT+CWMODE=1\r\n",1000,DEBUG); // modo de operação STA
-    Serial.println("- Resetando modulo...");
-    sendData("AT+RST\r\n",1000,DEBUG); // resetar módulo
-    Serial.println("- Conectando no roteador");
-    sendData("AT+CWJAP=\"Gabriel\",\"bi91654704\"\r\n", 5000, DEBUG); // conectar na rede wifi
+  //RFID
+  Serial.println("Configurando RFID");
+  mfrc522.PCD_Init();
+  Serial.println("Feito");
 
-    // initialize ESP module
-    WiFi.init(&Serial1);
+  Serial.println("- Configurando modulo...");
+  sendData("AT+CWMODE=1\r\n",1000,DEBUG); // modo de operação STA
+  Serial.println("- Resetando modulo...");
+  sendData("AT+RST\r\n",1000,DEBUG); // resetar módulo
+  Serial.println("- Conectando no roteador");
+  sendData("AT+CWJAP=\"Gabriel\",\"bi91654704\"\r\n", 5000, DEBUG); // conectar na rede wifi
 
-   // check for the presence of the shield
-    if (WiFi.status() == WL_NO_SHIELD) {
-      Serial.println("WiFi shield not present");
-      // don't continue
-      while (true);
-    }
+  // initialize ESP module
+  WiFi.init(&Serial1);
 
-    // attempt to connect to WiFi network
-    while (status != WL_CONNECTED) {
-      Serial.print("Attempting to connect to WPA SSID: ");
-      Serial.println(SSID);
-      // Connect to WPA/WPA2 network
-      status = WiFi.begin(SSID, PASSWORD);
-    }
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
 
-    // you're connected now, so print out the data
-    Serial.println("You're connected to the network");
+  // attempt to connect to WiFi network
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(SSID);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(SSID, PASSWORD);
+  }
 
-    //connect to MQTT server
-    mqttClient.setServer(server, PORT);
-    mqttClient.setCallback(callback); 
+  // you're connected now, so print out the data
+  Serial.println("You're connected to the network");
+
+  //connect to MQTT server
+  mqttClient.setServer(server, PORT);
+  mqttClient.setCallback(callback);
 }
 
-void callback(char* topic, byte* payload, unsigned int length) { 
+void callback(char* topic, byte* payload, unsigned int length) {
   int i = 0;
-<<<<<<< HEAD:rfid-mqtt/rfid-mqtt.ino
 
-//    Serial.print("Message arrived [");
-//    Serial.print(topic);
-//    Serial.println("] ");
-  
-  
-=======
->>>>>>> 80467243f5bb992124ad779c36d207021598e85a:rfid-mqtt/rfid-mqtt.ino
-  if (strcmp(topic,"julinho/rota")==0){
-    for (int i=0;i<length;i++) {
-      Serial.print((char)payload[i]);
-      
+  if (strcmp(topic, "julinho/rota") == 0){
+    for (int i = 0; i < length; i++) {
+      Serial.print((char) payload[i]);
     }
-    
+
     Serial.print(" - ");
     Serial.println("ROTA = ");
     Serial.println((char*)payload);
   }
-<<<<<<< HEAD:rfid-mqtt/rfid-mqtt.ino
-//
-//  if (strcmp(topic,"julinho/#")==0){
-//    String msg = "";
-//    Serial.print("Message arrived [");
-//    Serial.print(topic);
-//    Serial.println("] ");
-//    for (int i=0;i<length;i++) {
-//      Serial.print((char)payload[i]);
-//      msg += (char)payload[i];
-//      message_buff[i] = payload[i];
-//    }
-//    message_buff[i] = '\0';
-//    String msgString = String(message_buff);
-//    Serial.println("Payload: " + msgString);
-//  }
-=======
 
-  if (strcmp(topic,"julinho/dev")==0){
+  if (strcmp(topic, "julinho/dev")==0){
     String msg = "";
     Serial.print("Message arrived [");
     Serial.print(topic);
@@ -135,7 +101,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     String msgString = String(message_buff);
     Serial.println("Payload: " + msgString);
   }
->>>>>>> 80467243f5bb992124ad779c36d207021598e85a:rfid-mqtt/rfid-mqtt.ino
 }
 
 String sendData(const char* command, const int timeout, boolean debug){
@@ -167,8 +132,8 @@ void reconnect() {
       mqttClient.subscribe("julinho/#");
       //mqttClient.subscribe("julinho/dev");
 //        mqttClient.subscribe("julinho/#");
-      
-          } 
+
+          }
       else {
       Serial.print("failed, rc = ");
       Serial.print(mqttClient.state());
@@ -182,7 +147,7 @@ void reconnect() {
 void loop() {
 
       //  Procura tags;
-    if ( ! mfrc522.PICC_IsNewCardPresent()) { 
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
     }
 
     //  Lê a tag
@@ -192,31 +157,17 @@ void loop() {
     rfidUid += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
     rfidUid += String(mfrc522.uid.uidByte[i], HEX);
   }
-    
+
   // put your main code here, to run repeatedly:
-<<<<<<< HEAD:rfid-mqtt/rfid-mqtt.ino
-  //int value = random(1,10);
-  if (!mqttClient.connected()) {
-    reconnect();
-  } 
-  else {
-      
-=======
   if (!mqttClient.connected()) {
     reconnect();
   } else {
->>>>>>> 80467243f5bb992124ad779c36d207021598e85a:rfid-mqtt/rfid-mqtt.ino
-      rfidUid.toCharArray(message_buff, rfidUid.length() + 1);
-      mqttClient.publish("julinho/check", message_buff);
-      Serial.print("Message sended: ");
-      Serial.println(message_buff);
-<<<<<<< HEAD:rfid-mqtt/rfid-mqtt.ino
+    rfidUid.toCharArray(message_buff, rfidUid.length() + 1);
+    mqttClient.publish("julinho/check", message_buff);
+    Serial.print("Message sended: ");
+    Serial.println(message_buff);
+  }
 
-    }
-    mqttClient.loop();
-    return;
- 
-=======
-    }
->>>>>>> 80467243f5bb992124ad779c36d207021598e85a:rfid-mqtt/rfid-mqtt.ino
+  mqttClient.loop();
+  return;
 }
